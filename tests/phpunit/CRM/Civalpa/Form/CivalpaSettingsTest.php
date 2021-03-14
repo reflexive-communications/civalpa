9cb1618bcbc8c884eea5ce107057cdb25e8d0036
akosgarai <szlem01@gmail.com>
2021-03-14 17:02:29 +0100

    Implement test cases

--- tests/phpunit/CRM/Civalpa/Form/CivalpaSettingsTest.php
+++ tests/phpunit/CRM/Civalpa/Form/CivalpaSettingsTest.php
@@ -37,13 +37,24 @@ class CRM_Civalpa_Form_CivalpaSettingsTest extends \PHPUnit\Framework\TestCase i
         parent::tearDown();
     }
 
+    private function setupTestConfig() {
+        $config = new CRM_Civalpa_Config(E::SHORT_NAME);
+        self::assertTrue($config->update(self::TEST_SETTINGS), "Config update has to be successful.");
+    }
+
     /**
      * PreProcess test case with existing config.
      * Setup test configuration then call the function.
      * It shouldn't throw exception.
      */
     public function testPreProcessExistingConfig() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        try {
+            self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
+        } catch (Exception $e) {
+            self::fail("Shouldn't throw exception with valid db.");
+        }
     }
 
     /**
@@ -52,19 +63,30 @@ class CRM_Civalpa_Form_CivalpaSettingsTest extends \PHPUnit\Framework\TestCase i
      * It should throw exception.
      */
     public function testPreProcessMissingConfig() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        $config = new CRM_Civalpa_Config(E::SHORT_NAME);
+        $config->remove();
+        self::expectException(CRM_Core_Exception::class, "Invalid exception class.");
+        self::expectExceptionMessage(E::SHORT_NAME."_rules config invalid.", "Invalid exception message.");
+        self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
     }
 
     /**
      * Build quick form test case.
-     * Setup test configuration then call the function.
+     * Setup test configuration, preProcess then call the function.
      * It shouldn't throw exception.
-     * The number of form elements should be fine.
-     * The number of buttons should be fine.
      * The title should be set.
      */
     public function testBuildQuickForm() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
+        try {
+            self::assertEmpty($form->buildQuickForm());
+        } catch (Exception $e) {
+            self::fail("It shouldn't throw exception.");
+        }
+        self::assertEquals("Civalpa Settings", $form->getTitle(), "Invalid form title.");
     }
 
     /**
@@ -72,41 +94,108 @@ class CRM_Civalpa_Form_CivalpaSettingsTest extends \PHPUnit\Framework\TestCase i
      * It shouldn't throw exception.
      */
     public function testAddRules() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        try {
+            self::assertEmpty($form->addRules());
+        } catch (Exception $e) {
+            self::fail("It shouldn't throw exception.");
+        }
     }
 
     /**
-     * Config Validator test case with valid values.
-     */
-    public function testConfigValidatorValid() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
-    }
-
-    /**
-     * Config Validator test case with invalid values.
+     * Config Validator test cases.
      */
     public function testConfigValidatorInvalid() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $testData = [
+            [
+                "data" => [
+                    "textLineWidth" => "100",
+                    "htmlLineWidth" => "100",
+                ],
+                "expectedResult" => true,
+            ],
+            [
+                "data" => [
+                    "textLineWidth" => "100tt",
+                    "htmlLineWidth" => "100",
+                ],
+                "expectedResult" => ["textLineWidth" => "Only positiv integer values allowed."],
+            ],
+            [
+                "data" => [
+                    "textLineWidth" => "100",
+                    "htmlLineWidth" => "100tt",
+                ],
+                "expectedResult" => ["htmlLineWidth" => "Only positiv integer values allowed."],
+            ],
+            [
+                "data" => [
+                    "textLineWidth" => "100tt",
+                    "htmlLineWidth" => "100tt",
+                ],
+                "expectedResult" => ["htmlLineWidth" => "Only positiv integer values allowed.", "textLineWidth" => "Only positiv integer values allowed."],
+            ],
+        ];
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        foreach ($testData as $t) {
+            self::assertEquals($t["expectedResult"], $form->configValidator($t["data"]), "Should return the expected value.");
+        }
     }
 
     /**
      * Post Process test case. The values are the same.
      */
     public function testPostProcessNotChanged() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $_POST["debugMode"] = self::TEST_SETTINGS["debug-mode"];
+        $_POST["useTextRule"] = self::TEST_SETTINGS["text-line-width"]["use"];
+        $_POST["textLineWidth"] = self::TEST_SETTINGS["text-line-width"]["value"];
+        $_POST["useHtmlRule"] = self::TEST_SETTINGS["html-line-width"]["use"];
+        $_POST["htmlLineWidth"] = self::TEST_SETTINGS["html-line-width"]["value"];
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
+        try {
+            self::assertEmpty($form->postProcess());
+        } catch (Exception $e) {
+            self::fail("It shouldn't throw exception.");
+        }
     }
 
     /**
      * Post Process test case. The values are changed.
      */
     public function testPostProcessChanged() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $_POST["debugMode"] = !self::TEST_SETTINGS["debug-mode"];
+        $_POST["useTextRule"] = self::TEST_SETTINGS["text-line-width"]["use"];
+        $_POST["textLineWidth"] = self::TEST_SETTINGS["text-line-width"]["value"];
+        $_POST["useHtmlRule"] = self::TEST_SETTINGS["html-line-width"]["use"];
+        $_POST["htmlLineWidth"] = self::TEST_SETTINGS["html-line-width"]["value"];
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
+        try {
+            self::assertEmpty($form->postProcess());
+        } catch (Exception $e) {
+            self::fail("It shouldn't throw exception.");
+        }
     }
 
     /**
      * Set Default Values test case.
      */
     public function testSetDefaultValues() {
-        $this->markTestIncomplete("This test has not been implemented yet.");
+        $expectedDefaults = [
+            "debugMode" => self::TEST_SETTINGS["debug-mode"],
+            "useTextRule" => self::TEST_SETTINGS["text-line-width"]["use"],
+            "textLineWidth" => self::TEST_SETTINGS["text-line-width"]["value"],
+            "useHtmlRule" => self::TEST_SETTINGS["html-line-width"]["use"],
+            "htmlLineWidth" => self::TEST_SETTINGS["html-line-width"]["value"],
+        ];
+        $this->setupTestConfig();
+        $form = new CRM_Civalpa_Form_CivalpaSettings();
+        self::assertEmpty($form->preProcess(), "PreProcess supposed to be empty.");
+        self::assertEquals($expectedDefaults, $form->setDefaultValues(), "Invalid default values.");
     }
 }
